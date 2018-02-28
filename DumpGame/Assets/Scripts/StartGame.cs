945e04f4a68d1fc8b07d8b53b70b6f9ecb74ba7e
BradyJacobson <u0993342@utah.edu>
2018-02-27 19:38:34 -0700

    Moved items into folders
    Reorganized items so that code and scenes are in the proper locations.

---
+++ DumpGame/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string StageScene;
+
+    void OnMouseDown()
+    {
+        StageScene = "StageScene";
+        SceneManager.LoadScene(StageScene);
+    }
+}
---
+++ DumpGame/Assets/Scripts/UpFlag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpFlag : MonoBehaviour
+{
+    float a, b;
+
+    // Use this for initialization
+    void Start()
+    {
+        a = 14f;
+        b = 0.40f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a > 0)
+        {
+            transform.Translate(0, b, 0);
+            a -= b;
+        }
+        else
+        {
+            this.enabled = false;
+        }
+    }
+}
