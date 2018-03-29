969e067243bf4114ec9e000ffb128d9b27207428
BradyJacobson <u0993342@utah.edu>
2018-03-29 11:39:37 -0600

    Airport game update
    Updated the animation of the airport game

--- DumpGame/Assets/Scripts/GameAirport.cs
+++ DumpGame/Assets/Scripts/GameAirport.cs
@@ -4,47 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameAirport : MonoBehaviour {
-
-    public GameObject Immigrant, Dump, Curtain, ScoreKeeper;
-    public SpriteRenderer DumpSR;
-    public Sprite im;
+public class GameAirport : MonoBehaviour
+{
+    public GameObject Self, Curtain;
     public int Win;
-    public bool Clicked;
     public float T;
     public Text ScoreText, LivesText, RuleText, TimeText;
     public double tt;
-    
+
     void Start ()
     {
         ScoreText.enabled = false;
         LivesText.enabled = false;
         RuleText.enabled = false;
         Win = 0;
-        Clicked = Immigrant.GetComponent<ClickItem>().Clicked;
-        DumpSR = Dump.GetComponent<SpriteRenderer>();
-        DumpSR.enabled = false;
         T = PlayerPrefs.GetFloat("PTime");
         Curtain.GetComponent<UpFlag>().enabled = true;
         tt = T;
     }
-	
+
+    public void Results()
+    {
+        Win = 1;
+    }
+
 	void Update ()
     {
-        Clicked = Immigrant.GetComponent<ClickItem>().Clicked;
-        if (Clicked == true)
-        {
-            DumpSR.enabled = true;
-            Immigrant.GetComponent<SpriteRenderer>().sprite = im;
-            Win = 1;
-        }
-
         if (T < 0)
         {
             PlayerPrefs.SetInt("Result", Win);
             Curtain.GetComponent<DownFlag>().enabled = true;
             Curtain.GetComponent<PresentResults>().enabled = true;
-            Dump.GetComponent<GameAirport>().enabled = false;
+            Self.GetComponent<GameAirport>().enabled = false;
         }
         else
         {
