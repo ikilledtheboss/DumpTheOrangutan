b2cba46a121eb06aebafd566b4fd424f9d3a2824
BradyJacobson <u0993342@utah.edu>
2018-04-15 01:33:09 -0600

    Updated soda and vote
    The animations for voting and chugging should be implemented. The final
    screen after the vote is a placeholder.

--- DumpGame/Assets/Scripts/TimelineVote.cs
+++ DumpGame/Assets/Scripts/TimelineVote.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
-public class TimeLineController : MonoBehaviour
+public class TimelineVote : MonoBehaviour
 {
     public List<PlayableDirector> playableDirectors;
     public List<PlayableDirector> stopableDirectors;
     public List<TimelineAsset> timelines;
-    public  bool a = false;
+    public bool a = false;
+    public GameObject Button1, Button2, Button3;
+    public PlayableDirector pd;
 
     public void Play()
     {
+        Button2.SetActive(false);
         foreach (PlayableDirector playableDirector in playableDirectors)
         {
             playableDirector.Play();
+            pd = playableDirector;
         }
     }
 
@@ -27,12 +31,19 @@ public class TimeLineController : MonoBehaviour
             stopableDirector.Pause();
         }
     }
-
+    void Update()
+    {
+        if(pd.time > 23)
+        {
+            Button1.SetActive(true);
+            Button3.SetActive(false);
+        }
+    }
     public void PlayFromTimelines(int index)
     {
         TimelineAsset selectedAsset;
 
-        if(timelines.Count <= index)
+        if (timelines.Count <= index)
         {
             selectedAsset = timelines[timelines.Count - 1];
         }
