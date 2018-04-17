f9c894661f13dd6201d663e15102dfd9f26e444e
BradyJacobson <u0993342@utah.edu>
2018-04-17 13:42:21 -0600

    Created Butin game, updated Key and Earth game.
    Created the Butin game, changed some minor animation stuff in the Key
    and Earth game, and updated Present Results.

---
+++ DumpGame/Assets/Scripts/GameButin.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameButin : MonoBehaviour
+{
+    public GameObject Button1, Button2, Button3, Store, Curtain;
+    public int Progress, Win;
+    public float T;
+    public Text ScoreText, LivesText, RuleText, TimeText;
+    public double tt;
+
+    void Start()
+    {
+        ScoreText.enabled = false;
+        LivesText.enabled = false;
+        RuleText.enabled = false;
+        Button2.GetComponent<Button>().enabled = false;
+        Button3.GetComponent<Button>().enabled = false;
+        Progress = 1;
+        Win = 0;
+        T = PlayerPrefs.GetFloat("PTime");
+        Curtain.GetComponent<UpFlag>().enabled = true;
+        tt = T;
+    }
+
+    public void FirstClick()
+    {
+        Progress = 2;
+        Button1.GetComponent<Button>().enabled = false;
+        Button2.GetComponent<Button>().enabled = true;
+    }
+
+    public void SecondClick()
+    {
+        Progress = 3;
+        Button2.GetComponent<Button>().enabled = false;
+        Button3.GetComponent<Button>().enabled = true;
+        Button3.transform.Translate(0, 2.2f, 0);
+    }
+
+    public void ThirdClick()
+    {
+        Win = 1;
+        Progress = 4;
+        Button3.GetComponent<Button>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (T < 0)
+        {
+            PlayerPrefs.SetInt("Result", Win);
+            Curtain.GetComponent<DownFlag>().enabled = true;
+            Curtain.GetComponent<PresentResults>().enabled = true;
+            Store.GetComponent<GameButin>().enabled = false;
+        }
+        else
+        {
+            T = T - Time.deltaTime;
+        }
+    }
+}
--- DumpGame/Assets/Scripts/GameEarth.cs
+++ DumpGame/Assets/Scripts/GameEarth.cs
@@ -7,8 +7,9 @@ using UnityEngine.SceneManagement;
 public class GameEarth : MonoBehaviour
 {
 
-    public GameObject Self, earthone, earthlast, Curtain;
-    public int Win;
+    public GameObject Self, earthone, earthlast, earthchange, f1, f2, f3, f4 ,f5, Curtain;
+    public Sprite store, s1, s2, s3, end;
+    public int Progress, Win;
     public float T;
     public Text ScoreText, LivesText, RuleText, TimeText;
     public double tt;
@@ -19,6 +20,7 @@ public class GameEarth : MonoBehaviour
         LivesText.enabled = false;
         RuleText.enabled = false;
         Win = 0;
+        Progress = 1;
         T = PlayerPrefs.GetFloat("PTime");
         Curtain.GetComponent<UpFlag>().enabled = true;
         tt = T;
@@ -26,13 +28,47 @@ public class GameEarth : MonoBehaviour
 
     public void Results()
     {
+        Progress = 2;
         Win = 1;
+        Self.GetComponent<Button>().interactable = false;
         earthone.GetComponent<SpriteRenderer>().enabled = false;
-        earthlast.GetComponent<SpriteRenderer>().enabled = true;
+        earthchange.GetComponent<Animator>().enabled = true;
+        earthchange.GetComponent<SpriteRenderer>().enabled = true;
     }
 
     void Update()
     {
+        if (Progress == 2)
+        {
+            store = earthchange.GetComponent<SpriteRenderer>().sprite;
+            if(store == s1)
+            {
+                f1.GetComponent<Animator>().enabled = true;
+                f1.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else if (store == s2)
+            {
+                f2.GetComponent<Animator>().enabled = true;
+                f2.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else if (store == s3)
+            {
+                f3.GetComponent<Animator>().enabled = true;
+                f3.GetComponent<SpriteRenderer>().enabled = true;
+                f4.GetComponent<Animator>().enabled = true;
+                f4.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else if (store == end)
+            {
+                Progress = 3;
+                f5.GetComponent<Animator>().enabled = true;
+                f5.GetComponent<SpriteRenderer>().enabled = true;
+                earthchange.GetComponent<Animator>().enabled = false;
+                earthchange.GetComponent<SpriteRenderer>().enabled = false;
+                earthlast.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+
         if (T < 0)
         {
             PlayerPrefs.SetInt("Result", Win);
