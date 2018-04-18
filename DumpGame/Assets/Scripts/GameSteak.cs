25bae988c8c2113ee67671eade621a12aa02b5ea
BradyJacobson <u0993342@utah.edu>
2018-04-17 18:05:09 -0600

    Random game changes
    Random game changes omg

--- DumpGame/Assets/Scripts/GameSteak.cs
+++ DumpGame/Assets/Scripts/GameSteak.cs
@@ -10,8 +10,7 @@ public class GameSteak : MonoBehaviour
 {
 
 	public GameObject Steak, Ketchup, Curtain, ScoreKeeper;
-
-	public int Progress, Win, kval;
+	public int Win, kval;
 	public int collideCounter = 0; 
 	public float T, y1, x1;
 	public Text ScoreText, LivesText, RuleText, TimeText;
@@ -22,33 +21,33 @@ public class GameSteak : MonoBehaviour
 		ScoreText.enabled = false;
 		LivesText.enabled = false;
 		RuleText.enabled = false;
-		Progress = 0;
 		Win = 0;
-		//Pilesr = Pile.GetComponent<SpriteRenderer>();
 		kval = Ketchup.GetComponent<ClickDragItem>().Drag;
-		T = PlayerPrefs.GetFloat("PTime");
+		T = 6;
 		Curtain.GetComponent<UpFlag>().enabled = true;
 		tt = T;
 	}
 
 	void Update ()
 	{
-		if (T < 0)
+        if (collideCounter == 10)
+        {
+            Win = 1;
+        }
+
+        if (T < 0)
 		{
 			PlayerPrefs.SetInt("Result", Win);
 			Curtain.GetComponent<DownFlag>().enabled = true;
 			Curtain.GetComponent<PresentResults>().enabled = true;
-			// Pile.GetComponent<GameMoney>().enabled = false;
+		    Steak.GetComponent<GameSteak>().enabled = false;
 		}
 		else
 		{
 			T = T - Time.deltaTime;
 		}
-
-		if (collideCounter == 5) {
-			print ("yo");
-		}
 	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "collide") 
@@ -59,12 +58,5 @@ public class GameSteak : MonoBehaviour
 
 	public void AddCounter(){
 		++collideCounter;
-//		if (collideCounter == 5) {
-//			print ("hey");
-//		}
-//
 	}
-
-
-
 }
