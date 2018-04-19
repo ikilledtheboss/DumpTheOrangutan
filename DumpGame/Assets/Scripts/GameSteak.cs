a1725163328f9a707530a9eda543b886a2764e24
BradyJacobson <u0993342@utah.edu>
2018-04-19 03:19:25 -0600

    Steak game and possible quit button
    Updated the Steak game so it seems to work well. Also added what should
    be the quit button. Allegedly only works in compiled game.

--- DumpGame/Assets/Scripts/GameSteak.cs
+++ DumpGame/Assets/Scripts/GameSteak.cs
@@ -1,62 +1,71 @@
-﻿using System;
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class GameSteak : MonoBehaviour
 {
+    public GameObject self, Blurp, Curtain;
+    public int Win, tttt;
+    public int collideCounter;
+    public float T;
+    public Text ScoreText, LivesText, RuleText, TimeText;
+    public double tt;
+    public PlayableDirector Ketchup;
 
-	public GameObject Steak, Ketchup, Curtain, ScoreKeeper;
-	public int Win, kval;
-	public int collideCounter = 0; 
-	public float T, y1, x1;
-	public Text ScoreText, LivesText, RuleText, TimeText;
-	public double tt;
+    void Start()
+    {
+        ScoreText.enabled = false;
+        LivesText.enabled = false;
+        RuleText.enabled = false;
+        Win = 0;
+        T = PlayerPrefs.GetFloat("PTime");
+        Curtain.GetComponent<UpFlag>().enabled = true;
+        tt = T;
+    }
 
-	void Start ()
-	{
-		ScoreText.enabled = false;
-		LivesText.enabled = false;
-		RuleText.enabled = false;
-		Win = 0;
-		kval = Ketchup.GetComponent<ClickDragItem>().Drag;
-		T = 6;
-		Curtain.GetComponent<UpFlag>().enabled = true;
-		tt = T;
-	}
-
-	void Update ()
-	{
-        if (collideCounter == 10)
+    void Update()
+    {
+        if(collideCounter >= 10)
         {
+            Blurp.GetComponent<SpriteRenderer>().enabled = true;
+            Player();
             Win = 1;
         }
 
-        if (T < 0)
-		{
-			PlayerPrefs.SetInt("Result", Win);
-			Curtain.GetComponent<DownFlag>().enabled = true;
-			Curtain.GetComponent<PresentResults>().enabled = true;
-		    Steak.GetComponent<GameSteak>().enabled = false;
-		}
-		else
-		{
-			T = T - Time.deltaTime;
-		}
-	}
+        if(T < 0)
+        {
+            PlayerPrefs.SetInt("Result", Win);
+            Curtain.GetComponent<DownFlag>().enabled = true;
+            Curtain.GetComponent<PresentResults>().enabled = true;
+            self.GetComponent<GameSteak>().enabled = false;
+        }
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.tag == "collide") 
-		{
-			collideCounter++;
-		}
-	}
+        else
+        {
+            T = T - Time.deltaTime;
+        }
+    }
+    
+    void Player()
+    {
+        tttt = 12;
+        Ketchup.Play(); 
+    }
 
-	public void AddCounter(){
-		++collideCounter;
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "collide")
+        {
+            collideCounter++;
+        }
+    }
+
+    public void AddCounter()
+    {
+        ++collideCounter;
+    }
 }
